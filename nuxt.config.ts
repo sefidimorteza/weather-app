a0c2ff63e6f397423e46d736b654d743642239c9
sefidimorteza <144926408+sefidimorteza@users.noreply.github.com>
2024-04-02 01:19:10 +0330

    weatherAppWithNuxtjs

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  tailwindcss: {
+    exposeConfig: true,
+    viewer: true,
+    // and more...
+  }
+})
